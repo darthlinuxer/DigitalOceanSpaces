8a81aae380bc57758ea4b20e7227d7eac69869a7
Camilo Chaves <chaves.camilo@gmail.com>
2022-09-29 20:06:09 -0300

    Initial commit

---
+++ src/spaces/Spaces.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace DigitalOceanSpaces;
+public class Spaces
+{
+    public Spaces()
+    {
+
+    }
+
+    public static async Task<ListBucketsResponse> GetBuckets(IAmazonS3 client)
+    {
+        return await client.ListBucketsAsync();
+    }
+}
